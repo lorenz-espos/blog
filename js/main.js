7d24955ced8bdec6f37ef325f0ebde3c3c606c43
Lorenzo Esposito <lorenzoesposito@Air-di-Lorenzo.home>
2023-08-10 14:49:05 +0200

    progetti

--- js/main.js
+++ js/main.js
@@ -12,3 +12,33 @@ $(document).ready(function() {
         $('.carousel-item.active').css('opacity', '0.8');
     });
 });
+
+      function git_btn() {
+            // Effettua il reindirizzamento alla pagina di destinazione
+            window.open ('https://github.com/lorenz-espos/G2_Progetto_SAD.git', '_blank');
+        };
+
+        function git2_btn() {
+            // Effettua il reindirizzamento alla pagina di destinazione
+            window.open (' https://github.com/lorenz-espos/dino_2d_game.git', '_blank');
+        };
+
+        function git3_btn() {
+            // Effettua il reindirizzamento alla pagina di destinazione
+            window.open ('https://github.com/lorenz-espos/IEEE_UNINA_APP_2.0.git', '_blank');
+        };
+
+    function git_social(){
+        window.open ('https://github.com/lorenz-espos', '_blank');
+    }
+
+    function linkedin_social(){
+        window.open ('https://www.linkedin.com/in/lorenzo-esposito-4a1671194/', '_blank');
+    }
+
+    function instagram_social(){
+        window.open ('https://www.instagram.com/lorenz_espos/', '_blank');
+    }
+       
+
+    
